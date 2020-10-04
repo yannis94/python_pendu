a4a0d27d14b56b0a2e794c2270e19246fb516a2a
yannis94 <bengueci.yannis@gmail.com>
2020-10-04 16:34:44 +0200

    round creation, need player bdd

--- game.py
+++ game.py
@@ -6,41 +6,70 @@ import functions
     - gameplay ++ (try again, score board etc...)
 """
 #start round / set variable
-life = 3
-word = functions.word_random("words.txt")
-word_dic = {}
-word_mask = []
-completed = False
+def round(pseudo):
 
-for i in range(len(word)):
-    letter = word[i]
-    word_dic[i] = letter
-    word_mask.append(False)
+    def user_input(bol, answer):
+        while bol is False:
+            print("Invalid, you have to enter only one letter.")
+            answer = input("enter letter : ")
+            bol = functions.regex(answer)
+        
+        return answer
 
-#try player
-while life > 0 and completed is False:
-    word_result = ""
-    print("life : " + str(life))
 
-    input_j = input("enter letter : ")
-    test = functions.regex(input_j)
+    life = 3
+    word = functions.word_random("words.txt")
+    word_dic = {}
+    word_mask = []
+    completed = False
 
-   
-    while test is False:
-        print("Invalid try, you have to enter one and only a letter.")
-        input_j = input("enter letter : ")
-        test = functions.regex(input_j)    
+    for i in range(len(word)):
+        letter = word[i]
+        word_dic[i] = letter
+        word_mask.append(False)
+
+    #try player
+    while life > 0 and completed is False:
+        word_result = ""
+        print("life : " + str(life))
+
+        round_try = input("enter letter : ")
+        test = functions.regex(round_try)
+
+        input_j = user_input(test, round_try)
+
+        """
+        while test is False:
+            print("Invalid, you have to enter only one letter.")
+            input_j = input("enter letter : ")
+            test = functions.regex(input_j)    
+        """
+
+        input_test, life = functions.show_word(word_dic, word_mask, input_j, life)
+
+        for j in range(len(input_test)):
+            if input_test[j] is True:
+                word_result += word_dic[j]
+            else:
+                word_result += "*"
+
+        print(word_result)
+        completed = functions.is_complet(word_mask)
     
+    if completed is True:
+        print("Well done, you win !")
+    else:
+        print("You lose...")
+    
+    again = input("Wanna play again ? (y/n) : ")
+    new_game = functions.regex(again)
+    correct = user_input(new_game, again)
+    
+    if correct == "y":
+        round(pseudo)
+    else:
+        return
+    
+round("yaya")
 
-    input_test, life = functions.show_word(word_dic, word_mask, input_j, life)
-
-    for j in range(len(input_test)):
-        if input_test[j] is True:
-            word_result += word_dic[j]
-        else:
-            word_result += "*"
-
-    print(word_result)
-    completed = functions.is_complet(word_mask)
-
-print("End")
+print("End, thanks for playing")
